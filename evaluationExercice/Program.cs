310600e14ab683afafe5c4e7f06f9444d7ee74cf
chaimaaOufkiri <chaimaa.oufkiri@usmba.ac.ma>
2022-11-10 21:36:41 +0100

    Add project files.

---
+++ evaluationExercice/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evaluationExercice
+{
+    class Program
+    {
+        class evaluation
+        {
+            public DateTime datedebut;
+            public DateTime datefin;
+        }
+        class question
+        {
+            public float nbr;
+        }
+
+        class proposition
+        {
+            public string choix1;
+            public string choix2;
+            public string choix3;
+        }
+        class reponse
+        {
+            public string choix;
+        }
+
+        class degredeDertitude
+        {
+            public double degre;
+
+            public void verificationduDegre()
+            {
+
+            }
+        }
+        class reponseFausse
+        {
+
+        }
+        class reponseVraie
+        {
+
+        }
+        class noteperdue
+        {
+            public double n;
+            public void PerdreNote()
+            {
+
+            }
+        }
+        class notegagne
+        {
+            public double v;
+            public void ajoutdeNote()
+            {
+
+            }
+        }
+        class noteTotale
+        {
+            public double note;
+            public double calculdunote()
+            {
+                return note;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
